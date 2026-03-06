fa0021358ad1585fcbcf234122d205ef32f0e6a4
Rodrigo Costa <rodrigoc94@hotmail.com>
2026-03-06 16:45:45 -0300

    adjusts

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -62,6 +62,7 @@ export default function Navbar() {
                             <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
                         </DisclosureButton>
                     </div>
+
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex shrink-0 items-center">
                             <img
@@ -72,6 +73,7 @@ export default function Navbar() {
                             <span className="text-gray-200"> /Rodrigo Costa</span>
                         </div>
                     </div>
+
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
@@ -119,6 +121,7 @@ export default function Navbar() {
                                             {t('navbar.english')}
                                         </button>
                                     </MenuItem>
+                                    
                                     <MenuItem>
                                         <button onClick={() => handleLanguageChange('pt')} className="block w-full text-left px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden hover:bg-white/5">
                                             {t('navbar.portuguese')}
