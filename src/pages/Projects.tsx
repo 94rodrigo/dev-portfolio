f12963f04c002fd26ec59bb8d95c620ee4139490
Rodrigo Costa <rodrigoc94@hotmail.com>
2026-03-06 21:11:37 -0300

    url adjust

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
             title: "weather-ai-dashboard",
             description: "Weather Dashboard is a full-stack web application that provides real-time weather information and AI-powered insights for multiple locations. The project is built with a modern technology stack, supporting multiple languages for a global user experience.",
             href: "https://github.com/94rodrigo/weather-ai-dashboard",
-            imageSrc: "https://github-production-user-asset-6210df.s3.amazonaws.com/51063103/550016503-5d7261b4-1997-454b-8698-45a89e085e7c.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20260306%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260306T165020Z&X-Amz-Expires=300&X-Amz-Signature=b23e263b78c2eb1990b5b129385fbf6bb57bcef89510061c42ccdf724db9ff8f&X-Amz-SignedHeaders=host",
+            imageSrc: "https://private-user-images.githubusercontent.com/51063103/550016503-5d7261b4-1997-454b-8698-45a89e085e7c.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NzI4NDI1MDEsIm5iZiI6MTc3Mjg0MjIwMSwicGF0aCI6Ii81MTA2MzEwMy81NTAwMTY1MDMtNWQ3MjYxYjQtMTk5Ny00NTRiLTg2OTgtNDVhODllMDg1ZTdjLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNjAzMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjYwMzA3VDAwMTAwMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTVjOTIzZjUxMDUwNGIzOTQ5NmVkYjYyMzNkZTAzMGJjNDY4YTg0MzQ3NWY5Nzc4ZjIzYTMwZGYwN2U2MjQ1ZWImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.lL0kpMjCjMZo3NeZ91V1gEDkD4X1hO2-rYhu3jIP1xM",
             technologies: "Java, Typescript, React, Spring"
         },
         {
